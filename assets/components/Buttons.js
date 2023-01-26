a467010afcf1ce1eb7871ba1ecccc0c75ec75d7c
Vladislav <timaevvladislav@gmail.com>
2023-01-27 02:08:44 +0600

    add:refactor:buttons

--- assets/components/Buttons.js
+++ assets/components/Buttons.js
@@ -1,28 +1,68 @@
 import React from "react"
 import {Text, TouchableOpacity, View} from "react-native"
 
+import Ionicons from "@expo/vector-icons/Ionicons"
 import {styleTicket} from "../screens/Exam"
-import {useColor} from "../store/hooks/useColor"
 
-export const ButtonsExam = ({answers}) => {
-    const { handlerPressButton, isDisabled } = useColor()
+import {DrawerItem} from "@react-navigation/drawer"
+import {useColor} from "../store/hooks/useColor"
+import {DisableContext, DisableProvider} from "../context/disabled"
+import {Share} from "react-native"
+
+export const ButtonsExam = ({answers, item}) => {
+    const { handlerPressButton } = useColor()
 
     return (
-        answers.map((item, i) =>  (
-            <View style={styleTicket.container}>
-                <TouchableOpacity
-                    disabled={isDisabled}
-                    onPress={() => handlerPressButton(item, i)}
-                    style={[{backgroundColor: "#DDDDDD" }, styleTicket.item]}>
-                    <Text style={styleTicket.itemText}>
-                        {`${i + 1}.  ${item.answer_text}`}
-                    </Text>
-                </TouchableOpacity>
-            </View>
+        answers.map((answer, i) =>  (
+           <DisableProvider>
+               <DisableContext.Consumer>
+                   {(({isDisabled, setIsDisabled}) => (
+                       <View style={styleTicket.container} >
+                           <TouchableOpacity
+                               disabled={isDisabled}
+                               onPress={() => {
+                                   handlerPressButton(item, i)
+                                   setIsDisabled(true)
+                               }}
+                               style={[{backgroundColor: "#DDDDDD" }, styleTicket.item]}>
+                               <Text style={styleTicket.itemText}>
+                                   {`${i + 1}.  ${answer.answer_text}`}
+                               </Text>
+                           </TouchableOpacity>
+                       </View>
+                   ))}
+               </DisableContext.Consumer>
+           </DisableProvider>
         ))
     )
 }
 
-// disabled={buttonDisabled}
-// onPress={() => handlerOnPress(item, i)}
-// style={[{backgroundColor: buttonColor[i].color }, styleTicket.item]}
+export const ShareButton = () => {
+    const onShare = async () => {
+        try {
+            const result = await Share.share({
+                message: 'React Native | A framework for building native apps using React',
+            });
+            if (result.action === Share.sharedAction) {
+                if (result.activityType) {
+                    // shared with activity type of result.activityType
+                } else {
+                    // shared
+                }
+            } else if (result.action === Share.dismissedAction) {
+                // dismissed
+            }
+        } catch (error) {
+            alert(error.message)
+        }
+    };
+    return (
+        <DrawerItem
+            icon={({size, focused}) => (<Ionicons name="share-social-outline" size={size} color={focused ? '#7cc' : '#ccc'}/>)}
+            labelStyle={{color: "black"}}
+            label="Поделиться"
+            onPress={onShare}
+        />
+    )
+}
+
