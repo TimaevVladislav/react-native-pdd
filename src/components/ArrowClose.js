0f7d9653ed85ccd529d47199a48cb76e4acc167a
Vladislav <timaevvladislav@gmail.com>
2023-04-13 09:10:00 +0600

    add:color:clear

--- src/components/ArrowClose.js
+++ src/components/ArrowClose.js
@@ -1,11 +1,21 @@
-import React from "react"
+import React, {useContext, useEffect} from "react"
 import {useNavigation} from "@react-navigation/native"
 import {CountContext} from "../context/counter"
 import {TouchableOpacity, View} from "react-native"
 import Ionicons from "@expo/vector-icons/Ionicons"
+import {useColor} from "../hooks/useColor"
 
 export const ArrowClose = () => {
     const navigation = useNavigation()
+    const {colorId} = useColor()
+
+    useEffect(() => {
+        const clearColor = navigation.addListener("beforeRemove", () => {
+            colorId.current.map((color, id) => colorId.current[id] = "#DDDDDD")
+        })
+
+        return clearColor
+    }, [navigation])
 
     return (
         <CountContext.Consumer>
